f596804338a13c055f6e8c5446077f4837315678
Lukas A. Mueller <hello@lukasam.com>
2017-03-31 22:28:02 +0200

    Prepare for tests

---
+++ tests/pivotal.rs
@@ -0,0 +1,22 @@
+extern crate morse;
+
+use morse::{Plaintext, Morse, MorseType, MorseSubstiution};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn itu_morse_correct() {
+        let starting_word: Morse = "Samuel Morse".to_string().to_morse(MorseType::ITU);
+        println!("{}", starting_word);
+        assert_eq!(starting_word, "····−−−··−··−·· −−−−−·−·····")
+    }
+    #[test]
+    fn itu_differs() {
+        let word1: Morse = "Samuel Morse".to_string();
+        let word2: Morse = "Alex Crichton".to_string();
+        assert_ne!(word1.to_morse(MorseType::ITU), word2.to_morse(MorseType::ITU))
+    }
+    // TODO:
+    // Test that proves, substitutions aren't the same.
+}
