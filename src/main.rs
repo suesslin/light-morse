f6b248761ef936046ab264d035b01f3bf0d2c9c5
Lukas A. Mueller <hello@lukasam.com>
2017-04-01 13:08:43 +0200

    Add new tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ extern crate light_morse;
 use light_morse::*;
 
 fn main() {
-    let hehe: Morse = "Samuel Morse".to_string().to_morse(MorseType::ITU);
+    let hehe: Morse = "Morse".to_string().to_morse(MorseType::Gerke);
     println!("{}", hehe);
 }
--- tests/pivotal.rs
+++ tests/pivotal.rs
@@ -7,16 +7,33 @@ mod tests {
     use super::*;
     #[test]
     fn itu_morse_correct() {
-        let starting_word: Morse = "Samuel Morse".to_string().to_morse(MorseType::ITU);
-        println!("{}", starting_word);
-        assert_eq!(starting_word, "····−−−··−··−·· −−−−−·−·····")
+        let starting_word: Morse = "Morse".to_string().to_morse(MorseType::ITU);
+        assert_eq!(starting_word, "−−−−−·−·····")
     }
     #[test]
     fn itu_differs() {
-        let word1: Morse = "Samuel Morse".to_string();
-        let word2: Morse = "Alex Crichton".to_string();
+        let word1: Morse = "Morse".to_string();
+        let word2: Morse = "Telegram".to_string();
         assert_ne!(word1.to_morse(MorseType::ITU), word2.to_morse(MorseType::ITU))
     }
     // TODO:
     // Test that proves, substitutions aren't the same.
+    #[test]
+    fn itu_and_gerke_differ() {
+        let word1: Morse = "Morse".to_string();
+        assert_ne!(word1.to_morse(MorseType::ITU), word1.to_morse(MorseType::Gerke))
+    }
+
+    #[test]
+    fn gerke_morse_correct() {
+        let starting_word: Morse = "Morse".to_string().to_morse(MorseType::Gerke);
+        assert_eq!(starting_word, "−−·−····−·····")
+    }
+
+    #[test]
+    fn gerke_differs() {
+        let word1: Morse = "Morse".to_string();
+        let word2: Morse = "Telegram".to_string();
+        assert_ne!(word1.to_morse(MorseType::Gerke), word2.to_morse(MorseType::Gerke))
+    }
 }
